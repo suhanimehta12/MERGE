c1c668579351038702456777936efdd103a0baf3
suhanimehta12 <118454081+suhanimehta12@users.noreply.github.com>
2025-03-05 10:58:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+st.set_page_config(page_title="employee retention and recruitment model", layout="wide")
+
+
+st.title("employee retention and recruitment model")
+
+app_choice = st.radio("Select an Application:", ["employee retention", "employee recruitment"])
+
+
+if app_choice == "employee retention":
+    st.page_link("https://blank-app-ztexj6vr8xi.streamlit.app/", label="EMPLOYEE RETENTION APPLICATION")
+
+
+elif app_choice == "employee recruitment":
+    st.page_link("https://blank-app-r88p17uu5ea.streamlit.app/", label="EMPLOYEE RECRUITMENT APPLICATION")
